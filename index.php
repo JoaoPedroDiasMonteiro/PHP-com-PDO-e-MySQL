e4ab09e6a07c89a71c2440490c2fdf3c13111912
João Pedro <joaopedro453@hotmail.com>
2019-11-08 15:48:57 -0300

    Executando instruções SQL (Exec)

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=phpx_com_pdo';
+$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
 $usuario = 'root';
 $senha = '';
 
 try {
     $conexao = new PDO($dsn,$usuario,$senha);
+
+    $query = '
+    create table tb_usuarios(
+        id int not null primary key auto_increment,
+        nome varchar(50) not null,
+        email varchar(100) not null,
+        senha varchar(32) not null
+    )';
+    
+    $conexao->exec($query);
+
+    $query = '
+    insert into tb_usuarios(
+        nome, email, senha   
+    )values(
+        "Zé","zé@teste.com.br","132465"
+    )';
+
+    $conexao->exec($query);
 
 } catch (PDOException $e) {
     echo 'Erro: '. $e->getCode(). ' Mensagem: '.$e->getMessage();
